cf8f8b94889bf0bb2aa112b2788986bdcb123e67
gretmn102 <prince27@yandex.ru>
2016-09-19 13:35:36 +0300

    compilation ...

--- Server/Remade.fs
+++ Server/Remade.fs
@@ -167,37 +167,46 @@ type PlayingCard = { Rank:Rank; Suit:Suit }
 
 module Deck =
     type Deck = Deck of PlayingCard list
+    let suitVarCount = 4
+    let rankVarCount = 14
     let take = function
         | Deck [] -> failwith "card is no more in deck"
         | Deck (h::t) -> h, Deck t
     let init = 
-        [ for suit in [ 0..3 ] do
-            for rank in 0..13 -> { Rank = rank; Suit = suit } ]
+        [ for suit in 0..suitVarCount-1 do
+            for rank in 0..rankVarCount-1 -> { Rank = rank; Suit = suit } ]
         |> Deck
     let isEmpty = function
         | Deck [] -> true
         | _ -> false
 
 module Player = 
-    type Player = { Id:int; Cards:Set<PlayingCard> }
+    type Player = { Cards:Set<PlayingCard>; Chests:Set<Rank> }
     let takeFromDeck ({Cards = cards} as p) d =
         let (card, d') = Deck.take d
         {p with Cards = cards.Add card }, d'
-    let takeFromDeck2 ({Cards = cards} as p) d =
+    let addCard p card =
+        let cards = p.Cards |> Set.filter (fun x -> x.Rank = card.Rank)
+        if Set.count cards = Deck.suitVarCount - 1 then 
+            {p with Cards = p.Cards - cards}, Some card.Rank
+        else
+            {p with Cards = Set.add card p.Cards}, None
+    let takeFromDeck2 p d =
         let (card, d') = Deck.take d
-        {p with Cards = cards.Add card }, d', card
+        addCard p card, d', card
     let haveCards { Cards = cards } = Set.isEmpty cards |> not
     let haveCard card { Cards = cards } = Set.contains card cards
+
     let haveRank r { Cards = cards } =
         cards |> Set.exists (fun { Rank = x} -> x = r )
+    let filterRank r { Cards = cards } =
+        cards |> Set.filter (fun { Rank = x } -> x = r )
     let haveCount (r, count) { Cards = cards } =
         cards |> Set.filter (fun { Rank = x } -> x = r ) |> Set.count = count
-//    let haveSuit (r, suits) {Cards = cards} =
-//        let guessCards = Set [for s in suits -> { Rank = r; Suit = s }]
-//        Set.isSubset guessCards cards
+
     let give p1 p2 card =
         let p1 = {p1 with Cards = Set.remove card p1.Cards}
-        let p2 = {p2 with Cards = Set.add card p2.Cards}
+        let p2 = addCard p2 card
         p1, p2
 
 type Ask =
@@ -206,17 +215,21 @@ type Ask =
     | IsSuit of Set<Suit>
 type Info =
     | AddCard of PlayingCard
-    | RemoveCard of PlayingCard
+    //| RemoveCard of PlayingCard
 
-    | AskXOnYou  of Ask * PlayerId // противник спрашивает у тебя
+    | CompileChest of PlayerId * Rank
+
+    | AskXOnYou  of Ask * PlayerId
     | AskXOnY    of Ask * PlayerId * PlayerId
 
     | MoveYouOnX of PlayerId
-    | MoveXOnY   of PlayerId * PlayerId // кто-то на кого-то начинает ход
-    | MoveXOnYou of PlayerId // на тебя ходит
+    | MoveXOnY   of PlayerId * PlayerId
+    | MoveXOnYou of PlayerId
 
     | Success of bool
-    
+
+type PlayerInfo = { Id:PlayerId; CardsCount:int; Chests:Set<Rank> }
+
 type Answ =
     | Wait of PlayerId
     | EndGame
@@ -229,29 +242,46 @@ type Answ =
     | FailAnsw
     | Nil
 
+    /// <summary>На случай если один из игроков выйдет из игры и снова войдет, таких данных достаточно
+    /// чтобы продолжить игру.</summary>
+    | GameInfo of PlayerInfo list * Set<PlayingCard>
+
 type Inputs = 
     | RankInput of Rank
     | CountInput of int
     | SuitInput of Set<Suit>
+
 type Req = 
     | GetState of PlayerId
     | Input of PlayerId * Inputs
-    
+
+    /// <summary>Запрос на порядок, имена игроков, а так же кол-во карт у каждого.</summary>
+    | GetGameInfo of PlayerId
 open Continues
 type Msg =
     | Post of Req * AsyncReplyChannel<Answ>
 
-type State = { PCircle: PlayersCircleT; Deck: Deck.Deck; Players: Map<PlayerId, Player.Player> }
-type State2 = { MoveCircle: MoveCircleT; Deck: Deck.Deck; Players: Map<PlayerId, Player.Player> }
+type DataState = { Deck: Deck.Deck; Players: Map<PlayerId, Player.Player> }
+type State = { PCircle: PlayersCircleT; Data:DataState }
+type State2 = { MoveCircle: MoveCircleT; Data:DataState }
 
 let replyChannel (r:AsyncReplyChannel<_>) x = r.Reply x
 let receive (mailboxProc:MailboxProcessor<_>) = mailboxProc.Receive()
 
-let requester inbox interp =
+let requester inbox {Players = pls} interp =
     let rec loop () =
         async {
             let! msg = receive inbox
             match msg with
+            | Post(GetGameInfo pId, r) ->
+                let plsInfo = 
+                    Map.toList pls |> List.map (fun (id, p) -> 
+                        {PlayerInfo.Id = id;
+                         CardsCount = p.Cards |> Set.count
+                         PlayerInfo.Chests = p.Chests })
+                let answ = GameInfo(plsInfo, pls.[pId].Cards)
+                replyChannel r answ
+                return! loop()
             | Post(req, r) -> 
                 let (answ, ret) = interp req
                 replyChannel r answ
@@ -260,7 +290,7 @@ let requester inbox interp =
                 | None -> return! loop()
         }
     loop()
-let inputer inbox pId getStateAnsw inputState =
+let inputer inbox dataState pId getStateAnsw inputState =
     let interp = function
         | GetState pId' -> 
             if pId' = pId then getStateAnsw, None
@@ -268,128 +298,159 @@ let inputer inbox pId getStateAnsw inputState =
         | Input(pId', inputs) ->
             if pId' = pId then inputState inputs
             else FailAnsw, None
-    requester inbox interp
+        | _ -> FailAnsw, None
+    requester inbox dataState interp
 
-let informer inbox pId msg =
+let informer inbox dataState pId msg =
     let f = function
         | GetState pId' -> 
             if pId' = pId then msg, Some ()
             else Answ.Wait pId, None
         | _ -> FailAnsw, None
-    requester inbox f
+    requester inbox dataState f
 
-let loopMoveCircle (inbox:MailboxProcessor<_>) st =
-    let rec loopMoveCircle ({ MoveCircle = mc; Deck = d; Players = pls } as st) =
-        match mc with
-        | MoveCircleT.EndMoveCircle -> async { return st }
-        | MoveCircleT.GetMove((p1, p2, xs), f) -> 
-            async {
-                let informs answOnYou answXonY =
-                    async {
-                        do! informer inbox p2 (Info answOnYou)
-                        for x in xs do
-                            do! informer inbox x (Info answXonY)
-                    }
-                do! informs (MoveXOnYou p1) (MoveXOnY(p1, p2))
+let loopMoveCircle inbox st =
+    let getp pId (pls:Map<_,_>) = pls.[pId]
+    let getMove (p1, p2, xs) ({ Players = pls} as st) =
+        async {
+            let informs answOnYou answXonY =
+                async {
+                    do! informer inbox st p2 (Info answOnYou)
+                    for x in xs do
+                        do! informer inbox st x (Info answXonY)
+                }
+            do! informer inbox st p1 (Info (MoveYouOnX p2))
+            do! informs (MoveXOnYou p1) (MoveXOnY(p1, p2))
 
-                let! rankInput = 
-                    let f = function 
-                        | RankInput rank -> Nil, Some rank
-                        | _ -> FailAnsw, None
-                    inputer inbox p1 GetRank f
-                let ask = IsRank rankInput
-                do! informs (AskXOnYou(ask, p1)) (AskXOnY(ask, p1, p2))
-                let res = Player.haveRank rankInput pls.[p2]
-
-                let answ = Success res
-                do! informer inbox p1 (Info answ)
-                do! informs answ answ
-
-                if res then
-                    let! countInput =
-                        let f = function
-                            | CountInput count -> Nil, Some count
-                            | _ -> FailAnsw, None
-                        inputer inbox p1 GetCount f
-                    let ask = IsCount countInput
-                    do! informs (AskXOnYou(ask, p1)) (AskXOnY(ask, p1, p2))
-                    let res = Player.haveCount (rankInput, countInput) pls.[p2]
-
+            let informAllSucc res =
+                async {
                     let answ = Success res
-                    do! informer inbox p1 (Info answ)
+                    do! informer inbox st p1 (Info answ)
                     do! informs answ answ
-                    
-                    if res then
-                        let! suitInput =
-                            let f = function
-                                | SuitInput x -> Nil, Some x
-                                | _ -> FailAnsw, None
-                            inputer inbox p1 GetSuit f
-                        let ask = IsSuit suitInput
-                        do! informs (AskXOnYou(ask, p1)) (AskXOnY(ask, p1, p2))
-                        let guessCards = [for s in suitInput -> {Rank = rankInput; Suit = s}]
-                        
-                        let res = 
-                            guessCards |> List.forall (fun x -> Player.haveCard x pls.[p2])
-                        
-                        let answ = Success res
-                        do! informer inbox p1 (Info answ)
-                        do! informs answ answ
-                        if res then
-                            let give c pls = 
-                                async {
-                                    do! informer inbox p1 (Info.AddCard c |> Info)
-                                    do! informer inbox p2 (Info.RemoveCard c |> Info)
-                                    let (pd1, pd2) = Map.find p1 pls, Map.find p2 pls
-                                    let (pd2, pd1) = Player.give pd2 pd1 c
-                                    return Map.add p1 pd1 pls |> Map.add p2 pd2
-                                }
-                            failwith "not impl"
-                            return! loopMoveCircle { st with MoveCircle = f false }
-                        else
-                            return! loopMoveCircle { st with MoveCircle = f false }
-                    else
-                        return! loopMoveCircle { st with MoveCircle = f false }
-                else
-                    return! loopMoveCircle { st with MoveCircle = f false }
-            }
-        | MoveCircleT.PlayerHaveCards(pId, f) ->
-            loopMoveCircle {st with MoveCircle = Player.haveCards pls.[pId] |> f}
+                }
+            let maybe (x, f) = 
+                match x |> Async.RunSynchronously with
+                | None -> None
+                | Some x -> f x
+            let rankInputer () = 
+                let f = function
+                    | RankInput x -> Nil, Some x
+                    | _ -> FailAnsw, None
+                inputer inbox st p1 GetRank f
+            let countInputer () =
+                let f = function
+                    | CountInput x -> Nil, Some x
+                    | _ -> FailAnsw, None
+                inputer inbox st p1 GetCount f
+            let suitInputer () =
+                let f = function
+                    | SuitInput x -> Nil, Some x
+                    | _ -> FailAnsw, None
+                inputer inbox st p1 GetSuit f
+
+            let func inputFunc askType f =
+                async {
+                    let! input = inputFunc()
+                    let ask = askType input
+                    do! informs (AskXOnYou(ask, p1)) (AskXOnY(ask, p1, p2))
+                    match f input with
+                    | None -> 
+                        do! informAllSucc false
+                        return None
+                    | Some x -> 
+                        do! informAllSucc true
+                        return Some x
+                }
+
+            let result = 
+                let re = func rankInputer IsRank (fun x -> 
+                    let cards = Player.filterRank x (getp p2 pls)
+                    if Set.isEmpty cards then None else Some cards)
+                maybe(re, fun cards ->
+                    let r = 
+                        func countInputer IsCount (fun count ->
+                            if Set.count cards = count then Some cards else None)
+                    maybe(r, fun cards ->
+                        let f suits = 
+                            if suits |> Set.forall (fun s -> cards |> Set.exists(fun c -> c.Suit = s)) then
+                                Some cards
+                            else None
+                        func suitInputer IsSuit f
+                        |> Async.RunSynchronously)
+                )
+            match result with
+            | None -> return false, st
+            | Some cards ->
+                let give c ({Players = pls} as st) = 
+                    async {
+                        //do! informer inbox st p1 (Info.AddCard c |> Info)
+                        //do! informer inbox st p2 (Info.RemoveCard c |> Info)
+                        let (pd1, pd2) = Map.find p1 pls, Map.find p2 pls
+                        let (pd2, (pd1, chest)) = Player.give pd2 pd1 c
+                        match chest with 
+                        | None -> ()
+                        | Some rank -> 
+                            for p in p1::p2::xs do
+                                do! informer inbox st p (CompileChest(p1, rank) |> Info)
+                        return {st with Players = Map.add p1 pd1 pls |> Map.add p2 pd2}
+                    }
+                let st = 
+                    cards |> Set.fold (fun state x -> give x state |> Async.RunSynchronously) st
+                return true, st
+        }
+    let rec loopMoveCircle ({ MoveCircle = mc; Data = {Players = pls} as dataState } as st) =
+        async {
+            match mc with
+            | MoveCircleT.EndMoveCircle -> return st
+            | MoveCircleT.GetMove(x, fcont) -> 
+                let! (res, data) = getMove x dataState
+                return! loopMoveCircle {MoveCircle = fcont res; Data = data}
+            | MoveCircleT.PlayerHaveCards(pId, f) ->
+                return! loopMoveCircle {st with MoveCircle = Player.haveCards pls.[pId] |> f}        
+        }
+
     loopMoveCircle st
 
 let mail plsId =
-    MailboxProcessor.Start (fun inbox ->
-        let rec loopCircle ({ PCircle = pcl; Deck = d; Players = pls } as st) =
+    let loopCircle st inbox = 
+        let rec loop ({ PCircle = pcl; Data = {Deck = d; Players = pls} as dataState} as st) =
             let pUpdate id v = Map.add id v pls
             match pcl with
             | PlayersCircleT.DeckIsEmpty f -> 
-                loopCircle {st with PCircle = Deck.isEmpty d |> f}
+                loop {st with PCircle = Deck.isEmpty d |> f}
             | PlayersCircleT.End -> 
-                requester inbox (function _ -> Answ.EndGame, None)
-            | PlayersCircleT.MoveCircle(pl, f) as x ->
+                requester inbox dataState (function _ -> Answ.EndGame, None)
+            | PlayersCircleT.MoveCircle(pl, f) ->
                 async {
                     let! r = 
-                        loopMoveCircle inbox { MoveCircle = moveCircle pl; Deck = d; Players = pls }
-                    let st = { PCircle = f(); Deck = r.Deck; Players = r.Players }
-                    return! loopCircle st
+                        loopMoveCircle inbox { MoveCircle = moveCircle pl; Data = {Deck = d; Players = pls}}
+                    let st = { PCircle = f(); Data = r.Data }
+                    return! loop st
                 }
             | PlayersCircleT.PlayerHaveCards(pl, f) ->
-                loopCircle {st with PCircle = Player.haveCards pls.[pl] |> f}
+                loop {st with PCircle = Player.haveCards pls.[pl] |> f}
             | PlayersCircleT.PlayerTakeCardFromDeck(pId, f) ->
                 async {
-                    let (p, d, card) = Player.takeFromDeck2 pls.[pId] d
-                    do! informer inbox pId (Info(AddCard card))
-                    return! loopCircle {PCircle = f(); Deck = d; Players = pUpdate pId p}
+                    let ((p, chest), d, card) = Player.takeFromDeck2 pls.[pId] d
+                    match chest with
+                    | None ->
+                        do! informer inbox dataState pId (Info(AddCard card))
+                    | Some rank ->
+                        for p in plsId do
+                            do! informer inbox dataState p (Info(CompileChest(pId, rank)))
+                    return! loop {PCircle = f(); Data = {dataState with Players = pUpdate pId p} }
                 }
             | PlayersCircleT.Fail str -> failwith str
+        loop st
+    MailboxProcessor.Start (fun inbox ->
         let pCircleSt = playersCircle (List.ofSeq plsId)
-        let pls = plsId |> Set.map (fun x -> {Player.Id = x; Player.Cards = Set.empty})
+        let pls = List.init (Set.count plsId) (fun _ -> { Player.Cards = Set.empty; Player.Chests = Set.empty})
         let d = Deck.init
         let (d, pls) = 
             let f (d,pls) p =
                 let (p, d) = Player.takeFromDeck p d
                 (d, p::pls)
-            pls |> Set.fold f (d, [])
-        let pls = pls |> List.map (fun x -> x.Id, x) |> Map.ofList
-        let initSt = { PCircle = pCircleSt; Deck = d; Players = pls }
-        loopCircle initSt)
+            pls |> List.fold f (d, [])
+        let pls = List.zip (List.ofSeq plsId) pls |> Map.ofList
+        let initSt = { PCircle = pCircleSt; Data = {Deck = d; Players = pls}}
+        loopCircle initSt inbox)
--- Server/RemadeTest.fs
+++ Server/RemadeTest.fs
@@ -87,8 +87,69 @@ module Basic =
     open Remade
     
     let m = Remade.mail (set[1..3])
-    m.PostAndReply ((fun r -> Post(GetState 1, r)), 500) |> printfn "%A"
-    m.PostAndReply ((fun r -> Post(GetState 2, r)), 500) |> printfn "%A"
-    m.PostAndReply ((fun r -> Post(GetState 3, r)), 500) |> printfn "%A"
-    m.PostAndReply ((fun r -> Post(Input(2, RankInput 2), r)), 500) |> printfn "%A"
-    m.PostAndReply ((fun r -> Post(Input(2, CountInput 1), r)), 500) |> printfn "%A"
+    let get p = m.PostAndReply ((fun r -> Post(GetState p, r)), 500) |> printfn "%A"
+    let input p input = m.PostAndReply ((fun r -> Post(Input(p, input), r)), 500) |> printfn "%A"
+    get 1
+    get 2
+    get 3
+    input 1 (RankInput 2)
+    input 1 (CountInput 1)
+    input 1 (SuitInput (Set[0]))
+module SandBox =
+    let maybe (x, f) = Option.bind f x
+    
+(*
+                let! rankInput = 
+                    let f = function
+                        | RankInput x -> Nil, Some x
+                        | _ -> FailAnsw, None
+                    inputer inbox p1 GetRank f
+
+                let ask = IsRank rankInput
+                do! informs (AskXOnYou(ask, p1)) (AskXOnY(ask, p1, p2))
+                let res = Player.haveRank rankInput pls.[p2]
+                do! informAllSucc res
+
+                if res then
+                    let! countInput =
+                        let f = function
+                            | CountInput x -> Nil, Some x
+                            | _ -> FailAnsw, None
+                        inputer inbox p1 GetCount f
+                    let ask = IsCount countInput
+                    do! informs (AskXOnYou(ask, p1)) (AskXOnY(ask, p1, p2))
+                    let res = Player.haveCount (rankInput, countInput) pls.[p2]
+                    do! informAllSucc res
+                    
+                    if res then
+                        let! suitInput =
+                            let f = function
+                                | SuitInput x -> Nil, Some x
+                                | _ -> FailAnsw, None
+                            inputer inbox p1 GetSuit f
+                        let ask = IsSuit suitInput
+                        do! informs (AskXOnYou(ask, p1)) (AskXOnY(ask, p1, p2))
+                        let guessCards = [for s in suitInput -> {Rank = rankInput; Suit = s}]
+                        
+                        let res = 
+                            guessCards |> List.forall (fun x -> Player.haveCard x pls.[p2])
+                        do! informAllSucc res
+
+                        if res then
+                            let give c pls = 
+                                async {
+                                    do! informer inbox p1 (Info.AddCard c |> Info)
+                                    do! informer inbox p2 (Info.RemoveCard c |> Info)
+                                    let (pd1, pd2) = Map.find p1 pls, Map.find p2 pls
+                                    let (pd2, pd1) = Player.give pd2 pd1 c
+                                    return Map.add p1 pd1 pls |> Map.add p2 pd2
+                                }
+                            failwith "not impl"
+                            return! loopMoveCircle { st with MoveCircle = fcont false }
+                        else
+                            return! loopMoveCircle { st with MoveCircle = fcont false }
+                    else
+                        return! loopMoveCircle { st with MoveCircle = fcont false }
+                else
+                    return! loopMoveCircle { st with MoveCircle = fcont false }
+*)
